2d1cdeefbf403ff6bf1b841cd23731adcff97e2a
Robert Sutor <rssutor@gmail.com>
2025-07-27 13:46:56 -0400

    checkpoint

--- gitupdate.py
+++ gitupdate.py
@@ -4,6 +4,7 @@ from os_tools import delete_dir_if_present
 
 delete_dir_if_present("__pycache__")
 
+os.system("git gc")
 os.system("git add -A")
 os.system("git commit")
 os.system("git push -u origin main")
